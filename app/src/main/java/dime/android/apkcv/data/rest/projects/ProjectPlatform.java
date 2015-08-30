cf3a20822db8cd463977a6d73f66e037688bf8a5
Dimitar Kotevski <dime.kotevski@gmail.com>
2015-08-30 19:45:46 +0200

    Added the missing PYTHON position

--- app/src/main/java/dime/android/apkcv/data/rest/projects/ProjectPlatform.java
+++ app/src/main/java/dime/android/apkcv/data/rest/projects/ProjectPlatform.java
@@ -4,5 +4,5 @@ package dime.android.apkcv.data.rest.projects;
  * Created by dime on 30/08/15.
  */
 public enum ProjectPlatform {
-    ANDROID, IOS, JAVA, WEB
+    ANDROID, IOS, JAVA, WEB, PYTHON
 }
